a7c0210fc40d110de299261df7abe9b65f4e12d7
Jafir <jafir@iqbroadcast.tv>
2021-03-21 19:04:47 +0530

    Build 22

--- Larix/IQConnect-Bridging-Header.h
+++
@@ -1,4 +0,0 @@
-#import "StreamerEngineProxy.h"
-#import "AudioSessionHelper.h"
-#import "AudioUtils.h"
-#import "IpUtils.h"
--- Larix/ios9_support/AudioSessionHelper.h
+++
@@ -1,9 +0,0 @@
-#ifndef AudioSessionHelper_h
-#define AudioSessionHelper_h
-#import <AVFoundation/AVFoundation.h>
-
-@interface AudioSessionHelper: NSObject
-+ (BOOL) setAudioSessionWithError:(NSError **) error;
-@end
-
-#endif /* AudioSessionHelper_h */
